0c14bbd45a49070927c7913a4cb7417d2cfc247a
Reinout Vanhauwaert <reinout.vanhauwaert@student.ucll.be>
2017-05-08 15:55:15 +0200

    Aanmaken van spelers klassen en domain package waaronder ook domainexception klasse werd aangemaakt; basis

---
+++ GamesuiteHangman/src/domain/DomainException.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class DomainException extends RuntimeException {
+	DomainException(String message){
+		super(message);
+	}
+}
---
+++ GamesuiteHangman/src/domain/Speler.java
@@ -0,0 +1,52 @@
+package domain;
+
+public class Speler {
+	private String naam;
+	private int score;
+	
+	public Speler(String naam){
+		this.setNaam(naam);
+		this.setScore(0);
+	}
+	
+	public String getNaam() {
+		return naam;
+	}
+	public void setNaam(String naam) {
+		if(naam.isEmpty()||naam==null){
+			throw new DomainException("Naam mag niet leeg zijn");
+		}
+		else{
+		this.naam = naam;
+		}
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		if(score>=0){
+			throw new DomainException("Score mag niet negatief zijn.");
+		}
+		else{
+		this.score = score;
+		}
+	}
+	public void addToScore(int score){
+		setScore(this.score+score);
+	}
+	@Override
+	public boolean equals(Object object){
+		if( object instanceof Speler){
+			Speler s = (Speler) object;
+				if(this.getNaam().equals(s.getNaam())&&this.getScore()==s.getScore()){
+					return true;
+					}
+		}
+		return false;
+	}
+	public String toString(){
+		return this.getNaam()+" heeft als score: "+this.getScore();
+	}
+	
+	
+}
