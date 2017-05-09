505541354e3b5e3ed5d77f8b70fab101597c87e7
SametH <samet.halili@student.ucll.be>
2017-05-09 15:36:53 +0200

    [STORY 7] Aanpassing Rechthoek klasse en testen zodat het gebruik maak van de nieuwe Omhullende klasse

--- GamesuiteHangman/src/domain/Rechthoek.java
+++ GamesuiteHangman/src/domain/Rechthoek.java
@@ -3,13 +3,21 @@ package domain;
 public class Rechthoek extends Vorm
 {
 	Punt linkerBovenhoek;
+	Omhullende omhullende;
 	int breedte, hoogte;
 	
-	public Rechthoek(Punt linkerBovenhoek, int breedte, int hoogte)
+	public Rechthoek(Punt linkerBovenhoek, int breedte, int hoogte, Omhullende omhullende)
 	{
 		setLinkerBovenhoek(linkerBovenhoek);
 		setBreedte(breedte);
 		setHoogte(hoogte);
+		setOmhullende(omhullende);
+	}
+	
+	private void setOmhullende(Omhullende omhullende)
+	{
+		if(omhullende == null) throw new DomainException("Omhullend mag niet null zijn");
+		this.omhullende = omhullende;
 	}
 
 	private void setLinkerBovenhoek(Punt linkerBovenhoek) // fix
@@ -45,13 +53,21 @@ public class Rechthoek extends Vorm
 		return hoogte;
 	}
 	
+	public Omhullende getOmhullende()
+	{
+		return omhullende;
+	}
+	
 	@Override
 	public boolean equals(Object object)
 	{
 		if( object instanceof Rechthoek)
 		{
 			Rechthoek r = (Rechthoek) object;
-				if(this.getBreedte() == r.getBreedte() && this.getHoogte() == r.getHoogte() && this.getLinkerBovenhoek().equals(r.getLinkerBovenhoek()))
+				if(this.getBreedte() == r.getBreedte() 
+						&& this.getHoogte() == r.getHoogte() 
+						&& this.getLinkerBovenhoek().equals(r.getLinkerBovenhoek())
+						&& this.getOmhullende().equals(r.getOmhullende()))
 				{
 					return true;
 				}
@@ -61,7 +77,7 @@ public class Rechthoek extends Vorm
 	
 	public String toString()
 	{
-		return "Rechthoek: positie: (" + linkerBovenhoek.getX() + ", " + linkerBovenhoek.getY() + ") - breedte: " + this.getBreedte() + " - hoogte: " + this.getHoogte();
+		return "Rechthoek: positie: (" + linkerBovenhoek.getX() + ", " + linkerBovenhoek.getY() + ") - breedte: " + this.getBreedte() + " - hoogte: " + this.getHoogte() + "\n" + this.getOmhullende().toString();
 	}
 	
 
--- GamesuiteHangman/test/domain/RechthoekTest.java
+++ GamesuiteHangman/test/domain/RechthoekTest.java
@@ -10,18 +10,20 @@ public class RechthoekTest {
 	private Punt linkerBovenhoek;
 	private int breedte, hoogte;
 	private Rechthoek rechthoek;
+	private Omhullende omhullend;
 
 	@Before
 	public void setUp() {
 		linkerBovenhoek = new Punt(200, 200);
 		breedte = 20;
 		hoogte = 40;
-		rechthoek = new Rechthoek(linkerBovenhoek,breedte, hoogte);
+		omhullend = new Omhullende(linkerBovenhoek, breedte, hoogte);
+		rechthoek = new Rechthoek(linkerBovenhoek,breedte, hoogte, omhullend);
 	}
 
 	@Test
 	public void Rechthoek_moet_rechthoek_aanmaken_met_gegeven_linkerbovenhoek_breedte_en_hoogte() {
-		Rechthoek rechthoek = new Rechthoek(linkerBovenhoek,breedte, hoogte);
+		Rechthoek rechthoek = new Rechthoek(linkerBovenhoek,breedte, hoogte, omhullend);
 		assertEquals(linkerBovenhoek, rechthoek.getLinkerBovenhoek());
 		assertEquals(hoogte, rechthoek.getHoogte());
 		assertEquals(breedte, rechthoek.getBreedte());
@@ -29,52 +31,66 @@ public class RechthoekTest {
 	
 	@Test (expected = DomainException.class)
 	public void Rechthoek_Moet_exception_gooien_als_linkerbovenhoek_null()  {
-		new Rechthoek(null, breedte, hoogte);
+		new Rechthoek(null, breedte, hoogte, omhullend);
 	}
 	
 	@Test (expected = DomainException.class)
 	public void Rechthoek_Moet_exception_gooien_als_breedte_kleiner_dan_0()  {
-		new Rechthoek(linkerBovenhoek, -1, hoogte);
+		new Rechthoek(linkerBovenhoek, -1, hoogte, omhullend);
 	}
 	
 	@Test (expected = DomainException.class)
 	public void Rechthoek_Moet_exception_gooien_als_hoogte_kleiner_dan_0()  {
-		new Rechthoek(linkerBovenhoek, breedte, -1);
+		new Rechthoek(linkerBovenhoek, breedte, -1, omhullend);
 		
 	}
 
 	@Test (expected = DomainException.class)
 	public void Rechthoek_Moet_exception_gooien_als_breedte_gelijk_aan_0()  {
-		new Rechthoek(linkerBovenhoek, 0, hoogte);
+		new Rechthoek(linkerBovenhoek, 0, hoogte, omhullend);
 	}
 	
 	@Test (expected = DomainException.class)
 	public void Rechthoek_Moet_exception_gooien_als_hoogte_gelijk_aan_dan_0()  {
-		new Rechthoek(linkerBovenhoek, breedte, 0);
+		new Rechthoek(linkerBovenhoek, breedte, 0, omhullend);
+	}
+	
+	@Test (expected = DomainException.class)
+	public void rechthoek_moet_exception_gooien_als_omhullend_null()
+	{
+		new Rechthoek(linkerBovenhoek, breedte, hoogte, null);
 	}
 	
 	@Test
 	public void equals_moet_true_teruggeven_als_linkerbovenhoek_breedte_hoogte_gelijk_zijn(){
-		Rechthoek zelfdeRechthoek = new Rechthoek(linkerBovenhoek,breedte, hoogte);
+		Rechthoek zelfdeRechthoek = new Rechthoek(linkerBovenhoek,breedte, hoogte, omhullend);
 		assertTrue(rechthoek.equals(zelfdeRechthoek));
 	}
 	
 	@Test
 	public void equals_moet_false_teruggeven_als_linkerbovenhoek_verschilt(){
 		Punt andereLinkerBovenhoek = new Punt(100,100);
-		Rechthoek verschillendeRechthoek = new Rechthoek(andereLinkerBovenhoek, breedte, hoogte);
+		Rechthoek verschillendeRechthoek = new Rechthoek(andereLinkerBovenhoek, breedte, hoogte, omhullend);
 		assertFalse(rechthoek.equals(verschillendeRechthoek));
 	}
 	
 	@Test
 	public void equals_moet_false_teruggeven_als_breedte_verschilt(){
-		Rechthoek verschillendeRechthoek = new Rechthoek(linkerBovenhoek, breedte + 10, hoogte);
+		Rechthoek verschillendeRechthoek = new Rechthoek(linkerBovenhoek, breedte + 10, hoogte, omhullend);
 		assertFalse(rechthoek.equals(verschillendeRechthoek));
 	}
 	
 	@Test
 	public void equals_moet_false_teruggeven_als_hoogte_verschilt(){
-		Rechthoek verschillendeRechthoek = new Rechthoek(linkerBovenhoek, breedte, hoogte + 10);
+		Rechthoek verschillendeRechthoek = new Rechthoek(linkerBovenhoek, breedte, hoogte + 10, omhullend);
+		assertFalse(rechthoek.equals(verschillendeRechthoek));
+	}
+	
+	@Test
+	public void equals_moet_false_teruggeven_als_omhullend_verschilt()
+	{
+		Omhullende omhullendeVerschillend = new Omhullende(linkerBovenhoek, breedte+10, hoogte+10);
+		Rechthoek verschillendeRechthoek = new Rechthoek(linkerBovenhoek, breedte, hoogte, omhullendeVerschillend);
 		assertFalse(rechthoek.equals(verschillendeRechthoek));
 	}
 	
