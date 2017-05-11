abbb4531bdf43bca57eb109c91eb70942f64ef08
SametH <samet.halili@student.ucll.be>
2017-05-11 03:30:02 +0200

    [ALL] Herschrijving van de PictionaryUI klasse
    -Foute aanmaking van de vormen geven een error box terug
    -Menu wordt nu met ifs en elses behandelt
    -Alle functies die vormen aanmaken werken deftig
    -Code cleanup

--- GamesuiteHangman/src/ui/PictionaryUi.java
+++ GamesuiteHangman/src/ui/PictionaryUi.java
@@ -4,133 +4,217 @@ import javax.swing.JOptionPane;
 
 import domain.*;
 
-public class PictionaryUi {
+public class PictionaryUi 
+{
 	private Speler speler;
+	private Tekening newTekening;
+	private Object[] shapes = {"Cirkel", "Driehoek", "Lijnstuk", "Rechthoek"};
 
-	public PictionaryUi(Speler speler) {
+	public PictionaryUi(Speler speler) 
+	{
+		setSpeler(speler);
+	}
+	
+	private void setSpeler(Speler speler)
+	{
+		if(speler == null) throw new DomainException("Speler kan niet null zijn");
+		this.speler = speler;
 	}
 
-	public void ShowMenu() {
-		try {
-			// boolean running = true;
-			// while (running) {
-			Object[] shapes = { "Punt", "Cirkel", "Rechthoek", "Driehoek" };
-			Object keuze = JOptionPane.showInputDialog(null, "Wat wilt u tekenen?", "input",
-					JOptionPane.INFORMATION_MESSAGE, null, shapes, null);
-
-			// if (keuze == null) {
-			// running = false;
-			// } else {
-			switch (keuze.toString()) {
-			case "Punt":
-				voegPuntToe();
-				break;
-
-			case "Cirkel":
-				voegCirkelToe();
-				break;
-
-			case "Rechthoek":
-				voegRechthoekToe();
-				break;
-
-			case "Driehoek":
-				voegDriehoekToe();
-				break;
-			// }
+	public void showMenu()
+	{
+		JOptionPane.showMessageDialog(null,"...heeft als score: " + speler.getScore());
+		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		newTekening = voegToeNaamTekening();
+		if(newTekening == null) return;
+		newVormMaken();
+		
+	}
+	
+	private Tekening voegToeNaamTekening()
+	{
+		Tekening newTekening = null;
+		String naam = "";
+		
+		
+		while(naam.trim().isEmpty())
+		{
+			naam = JOptionPane.showInputDialog("Geef de naam van de tekening:");
+			if(naam == null) break;
+			try
+			{
+				newTekening = new Tekening(naam);
 			}
-			// }
-		} catch (Exception e) {
-			// JOptionPane.showMessageDialog(null, e.getMessage(), "Error",
-			// JOptionPane.ERROR_MESSAGE);
-		}
-
-	}
-
-	private Punt voegPuntToe() {
-
-		String xString = JOptionPane.showInputDialog("X-coordinaat van het punt:");
-		int x = Integer.parseInt(xString);
-
-		String yString = JOptionPane.showInputDialog("Y-coordinaat van het punt:");
-		int y = Integer.parseInt(yString);
-
-		Punt p = new Punt(x, y);
-		JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: (" + x + ", " + y + ")");
-		return p;
-	}
-
-	private void voegCirkelToe() {
-		boolean isFout = true;
-		Cirkel c = null;
-
-		while (isFout) {
-			try {
-
-				Punt p = voegPuntToe();
-
-				String Sradius = JOptionPane.showInputDialog("Radius van de cirkel:");
-				int radius = Integer.parseInt(Sradius);
-
-				c = new Cirkel(p, radius);
-				isFout = false;
-
-			} catch (DomainException e) {
+			catch(DomainException e)
+			{
 				JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-				isFout = true;
+				naam = "";
 			}
 		}
-
-		JOptionPane.showMessageDialog(null, "U heeft een correcte cirkel aangemaakt: " + c.toString());
+		
+		return newTekening;
+		
+	}
+	
+	private Punt newPuntMaken()
+	{
+		Punt newPunt = null;
+		while(newPunt == null)
+		{
+			String xCoordinaatStr = JOptionPane.showInputDialog("Geef de X coordinaat in :");
+			String yCoordinaatStr = JOptionPane.showInputDialog("Geef de Y coordinaat in :");
+			if(xCoordinaatStr == null || yCoordinaatStr == null) break;
+			int xCoordinaat = Integer.parseInt(xCoordinaatStr);
+			int yCoordinaat = Integer.parseInt(yCoordinaatStr);
+			try
+			{
+				newPunt = new Punt(xCoordinaat, yCoordinaat);
+			}
+			catch(DomainException e)
+			{
+				JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+				newPunt = null;
+			}
+		}
+		return newPunt;
+	}
+	
+	private void newVormMaken()
+	{
+		Object keuze = JOptionPane.showInputDialog(null, "Wat wilt u tekenen", "input", JOptionPane.INFORMATION_MESSAGE, null, shapes, null);
+		
+		if(keuze == null) return;
+		
+		if(keuze.equals("Cirkel"))
+		{
+			Cirkel newCirkel = voegCirkelToe();
+			if(newCirkel == null) return;
+			JOptionPane.showMessageDialog(null, "U heeft een correcte cirkel aangemaakt: \n" + newCirkel.toString(), speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		} 
+		else if(keuze.equals("Rechthoek"))
+		{
+			Rechthoek newRechthoek = voegRechthoekToe();
+			if(newRechthoek == null) return;
+			JOptionPane.showMessageDialog(null, "U heeft een correcte rechthoek aangemaakt: \n" + newRechthoek.toString(), speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		} 
+		else if(keuze.equals("Driehoek"))
+		{
+			Driehoek newDriehoek = voegDriehoekToe();
+			if(newDriehoek == null) return;
+			JOptionPane.showMessageDialog(null, "U heeft een correcte driehoek aangemaakt: \n" + newDriehoek.toString(), speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		} 
+		else if(keuze.equals("Lijnstuk"))
+		{
+			LijnStuk newLijnstuk = voegLijnStukToe();
+			if(newLijnstuk == null) return;
+			JOptionPane.showMessageDialog(null, "U heeft een correcte lijnstuk aangemaakt: \n" + newLijnstuk.toString(), speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		}
 	}
 
-	private void voegRechthoekToe() {
-		boolean isFout = true;
-		Rechthoek r = null;
-
-		while (isFout) {
-			try {
-
-				Punt bovenhoek = voegPuntToe();
-
-				String breedte = JOptionPane.showInputDialog("Breedte van de rechthoek:");
-				String hoogte = JOptionPane.showInputDialog("Hoogte van de rechthoek:");
-
-				int iBreedte = Integer.parseInt(breedte);
-				int iHoogte = Integer.parseInt(hoogte);
-
-				r = new Rechthoek(bovenhoek, iBreedte, iHoogte);
-				isFout = false;
-
-			} catch (DomainException e) {
-				JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-				isFout = true;
+	private Cirkel voegCirkelToe() 
+	{
+		Cirkel newCirkel = null;
+		
+		while(newCirkel == null)
+		{
+			try
+			{
+				JOptionPane.showMessageDialog(null, "Geef nu de x en y waarde van de middelpunt", null, JOptionPane.INFORMATION_MESSAGE);
+				Punt middelPunt = newPuntMaken();
+				String radiusStr = JOptionPane.showInputDialog("Geef de radius in :");
+				if(middelPunt == null || radiusStr == null) return null;
+				int radius = Integer.parseInt(radiusStr);
+				newCirkel = new Cirkel(middelPunt, radius);
 			}
-
+			catch(DomainException e)
+			{
+				JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+				newCirkel = null;
+			}
 		}
-
-		JOptionPane.showMessageDialog(null, "U heeft een correcte rechthoek aangemaakt: " + r.toString());
+		
+		return newCirkel;
 	}
 
-	private void voegDriehoekToe() {
-		boolean isFout = true;
-		Driehoek d = null;
-
-		while (isFout) {
-			try {
-				Punt p1 = voegPuntToe();
-				Punt p2 = voegPuntToe();
-				Punt p3 = voegPuntToe();
-				// hier moet nog een omhullende driehoek bij komen.
-
-				d = new Driehoek(p1, p2, p3);
-				isFout = false;
-			} catch (DomainException e) {
+	private Rechthoek voegRechthoekToe() 
+	{
+		Rechthoek newRechthoek = null;
+		
+		while(newRechthoek == null)
+		{
+			try
+			{
+				JOptionPane.showMessageDialog(null, "Geef nu de x en y waarde van de linker boven hoek", null, JOptionPane.INFORMATION_MESSAGE);
+				Punt linkerBovenHoek = newPuntMaken();
+				String breedteStr = JOptionPane.showInputDialog("Geef de breedte in :");
+				String hoogteStr = JOptionPane.showInputDialog("Geef de hoogte in :");
+				if(linkerBovenHoek == null || breedteStr == null || hoogteStr == null) return null;
+				int breedte = Integer.parseInt(breedteStr);
+				int hoogte = Integer.parseInt(hoogteStr);
+				
+				newRechthoek = new Rechthoek(linkerBovenHoek, breedte, hoogte);
+			}
+			catch(DomainException e)
+			{
 				JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-				isFout = true;
+				newRechthoek = null;
 			}
 		}
-		JOptionPane.showMessageDialog(null, "U heeft een correcte driehoek aangemaakt: " + d.toString());
+		return newRechthoek;
+	}
+
+	private Driehoek voegDriehoekToe() 
+	{
+		Driehoek newDriehoek = null;
+		
+		while(newDriehoek == null)
+		{
+			try
+			{
+				JOptionPane.showMessageDialog(null, "Geef nu de x en y waarde van de eerste hoek", null, JOptionPane.INFORMATION_MESSAGE);
+				Punt eersteHoek = newPuntMaken();
+				JOptionPane.showMessageDialog(null, "Geef nu de x en y waarde van de tweede hoek", null, JOptionPane.INFORMATION_MESSAGE);
+				Punt tweedeHoek = newPuntMaken();
+				JOptionPane.showMessageDialog(null, "Geef nu de x en y waarde van de derde hoek", null, JOptionPane.INFORMATION_MESSAGE);
+				Punt derdeHoek = newPuntMaken();
+				if(eersteHoek == null || tweedeHoek == null || derdeHoek == null) return null;
+				
+				newDriehoek = new Driehoek(eersteHoek, tweedeHoek, derdeHoek);
+			}
+			catch(DomainException e)
+			{
+				JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+				newDriehoek = null;
+			}
+		}
+		
+		return newDriehoek;
+	}
+	
+	private LijnStuk voegLijnStukToe()
+	{
+		LijnStuk newLijnStuk = null;
+		
+		while(newLijnStuk == null)
+		{
+			try
+			{
+				JOptionPane.showMessageDialog(null, "Geef nu de x en y waarde van de eerste hoek", null, JOptionPane.INFORMATION_MESSAGE);
+				Punt eerstePunt = newPuntMaken();
+				JOptionPane.showMessageDialog(null, "Geef nu de x en y waarde van de tweede hoek", null, JOptionPane.INFORMATION_MESSAGE);
+				Punt tweedePunt = newPuntMaken();
+				if(eerstePunt == null || tweedePunt == null) return null;
+				
+				newLijnStuk = new LijnStuk(eerstePunt, tweedePunt);
+			}
+			catch(DomainException e)
+			{
+				JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+				newLijnStuk = null;
+			}
+		}
+		
+		return newLijnStuk;
 	}
 
 }
