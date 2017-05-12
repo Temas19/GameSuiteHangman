5a5e51d15b1796cc5d314d290cb8138673f0ec82
Lorenz Borgions <lorenz.borgions@live.be>
2017-05-12 11:32:16 +0200

    Aanmaken + schrijven van methodes HangMan klasse

--- GamesuiteHangman/src/domain/HangMan.java
+++ GamesuiteHangman/src/domain/HangMan.java
@@ -1,5 +1,49 @@
 package domain;
 
 public class HangMan {
+	
+	private Speler speler;
+	private WoordenLijst woordenlijst;
 
+	public HangMan (Speler speler, WoordenLijst woordenlijst){
+		setSpeler(speler);
+		setWoordenLijst(woordenlijst);
+	}
+	
+	public String getHint(){
+		return null;
+	}
+	
+	public void setSpeler(Speler speler){
+		if(speler == null){
+			throw new DomainException("Speler mag niet null zijn!");
+		}
+	}
+	
+	public void setWoordenLijst(WoordenLijst woordenlijst){
+		if(woordenlijst == null){
+			throw new DomainException("Woordenlijst mag niet null zijn!");
+		}
+	}
+	
+	public Speler getSpeler(){
+		return null;
+	}
+	
+	public TekeningHangMan getTekening(){
+		return null;
+	}
+	
+	public void raad(char letter){
+		
+	}
+	
+	public boolean isGameOver(){
+		return true;
+	}
+	
+	public boolean isGewonnen(){
+		return true;
+	}
+	
 }
